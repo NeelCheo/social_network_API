676c6c5f991b7cc3e7337c61ad9bc5e0617c6f7b
NeelCheo <73446815+NeelCheo@users.noreply.github.com>
2023-05-16 17:17:27 -0400

    added more functions to the controllers/routes and fixed issues till the server responded

--- controllers/thought.js
+++ controllers/thought.js
@@ -1,23 +1,128 @@
-// Add a reaction to a thought's reactions array
-const addReaction = (req, res) => {
-  Thought.findByIdAndUpdate(
-    { _id: req.params.thoughtId },
-    { $push: { reactions: req.body } },
+const { Thought, User } = require('../models');
+
+
+// get all thoughts
+const getAllThoughts = (req, res) => {
+  Thought.find({})
+    .then(dbThoughtData => res.json(dbThoughtData))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+}
+
+// get a single thought by id
+const getThoughtById = ({ params }, res) => {
+  Thought.findOne({ _id: params.id })
+    .then(dbThoughtData => {
+      // If no thought is found, send 404
+      if (!dbThoughtData) {
+        res.status(404).json({ message: 'No thought found with this id!' });
+        return;
+      }
+      res.json(dbThoughtData);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(400).json(err);
+    });
+}
+
+// create thought
+const createThought = ({ body }, res) => {
+  Thought.create(body)
+    .then(({ _id }) => {
+      return User.findOneAndUpdate(
+        { _id: body.userId },
+        { $push: { thoughts: _id } },
+        { new: true }
+      );
+    })
+    .then(dbUserData => {
+      if (!dbUserData) {
+        res.status(404).json({ message: 'No user found with this id!' });
+        return;
+      }
+      res.json(dbUserData);
+    })
+    .catch(err => res.json(err));
+}
+
+// update thought
+const updateThought = ({ params, body }, res) => {
+  Thought.findOneAndUpdate(
+    { _id: params.id },
+    body,
     { new: true, runValidators: true }
   )
-    .then(dbThoughtData => res.json(dbThoughtData))
+    .then(dbThoughtData => {
+      if (!dbThoughtData) {
+        res.status(404).json({ message: 'No thought found with this id!' });
+        return;
+      }
+      res.json(dbThoughtData);
+    })
     .catch(err => res.json(err));
-};
+}
 
-// Remove a reaction from a thought's reactions array
-const removeReaction = (req, res) => {
-  Thought.findByIdAndUpdate(
-    { _id: req.params.thoughtId },
-    { $pull: { reactions: { reactionId: req.params.reactionId } } },
+// delete thought
+const deleteThought = ({ params }, res) => {
+  Thought.findOneAndDelete({ _id: params.id })
+    .then(deletedThought => {
+      if (!deletedThought) {
+        return res.status(404).json({ message: 'No thought with this id!' });
+      }
+      return User.findOneAndUpdate(
+        { thoughts: params.id },
+        { $pull: { thoughts: params.id } },
+        { new: true }
+      );
+    })
+    .then(dbUserData => {
+      if (!dbUserData) {
+        res.status(404).json({ message: 'No user found with this id!' });
+        return;
+      }
+      res.json(dbUserData);
+    })
+    .catch(err => res.json(err));
+}
+
+// add reaction
+const addReaction = ({ params, body }, res) => {
+  Thought.findOneAndUpdate(
+    { _id: params.thoughtId },
+    { $push: { reactions: body } },
+    { new: true, runValidators: true }
+  )
+    .then(dbThoughtData => {
+      if (!dbThoughtData) {
+        res.status(404).json({ message: 'No thought found with this id!' });
+        return;
+      }
+      res.json(dbThoughtData);
+    })
+    .catch(err => res.json(err));
+}
+
+// remove reaction
+const removeReaction = ({ params }, res) => {
+  Thought.findOneAndUpdate(
+    { _id: params.thoughtId },
+    { $pull: { reactions: { reactionId: params.reactionId } } },
     { new: true }
   )
     .then(dbThoughtData => res.json(dbThoughtData))
     .catch(err => res.json(err));
-};
+}
 
-module.exports = { addReaction, removeReaction };
+
+module.exports = {
+  getAllThoughts,
+  getThoughtById,
+  createThought,
+  updateThought,
+  deleteThought,
+  addReaction,
+  removeReaction
+};
--- server.js
+++ server.js
@@ -1,23 +1,14 @@
 const express = require('express');
-const mongoose = require('mongoose');
+const db = require('./config/connection');
+
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-// Add middleware here
-app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-// Connect to the MongoDB
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/social-network-api', {
-  useFindAndModify: false,
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-});
-
-// Log mongo queries that are being run
-mongoose.set('debug', true);
-
-// Import routes
 app.use(require('./routes'));
 
-app.listen(PORT, () => console.log(`Connected on localhost:${PORT}`));
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+app.listen(PORT, () => console.log(`🌍 Connected on localhost:${PORT}`));
